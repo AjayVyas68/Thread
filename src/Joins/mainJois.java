619955e6b748ec1151114f31657257be242aeb60
Nihuu <ajayvyas625@gmail.com>
2021-08-02 17:35:18 +0530

    Thread

--- src/Joins/mainJois.java
+++ src/Joins/mainJois.java
@@ -2,13 +2,26 @@ package Joins;
 
 public class mainJois {
     public static void main(String[] args) {
-        var t=new DB1Runnable();
-        var t2=new DB2Runnable();
-        var t3=new Thread(t);
-        var t4=new Thread(t2);
-
+        var
+                t =
+                new DB1Runnable();
+        var
+                t2 =
+                new DB2Runnable();
+        var
+                t3 =
+                new Thread(t);
+        var
+                t4 =
+                new Thread(t2);
         t3.start();
         t4.start();
+        try {
+            t3.join();
+            t4.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("processing");
 
 
